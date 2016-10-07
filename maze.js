b369844ea78d47303f88a31a60a789f6478b7793
Alton Bodley <altonbodley@gmail.com>
2016-10-07 16:29:10 -0500

    Ex.1
    Top left wall turns red on mouseover

---
+++ maze.js
@@ -0,0 +1,8 @@
+
+window.onload = function(){
+		var el=document.getElementById('boundary1');
+		 el.addEventListener("mouseover",function(){	
+			el.setAttribute('class','boundary youlose');
+	});
+
+} 
