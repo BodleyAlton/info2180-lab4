5c1503947c78b5771e04bbbc41bf1386a2aabec4
Alton Bodley <altonbodley@gmail.com>
2016-10-11 14:06:24 -0500

    Ex. 3
    Alerts with end status; whether player won or lost the game.

--- maze.js
+++ maze.js
@@ -1,24 +1,47 @@
-
+var l=0//l=lose; this variable keeps track of whether the boders her touched
 window.onload = function(){
-		var el=document.getElementById('boundary1');
-		 el.addEventListener("mouseover",function(){
-		 	el.setAttribute('class','boundary youlose');
+		ex1();
+		ex2();
+		//ex3();
+}
+
+function ex1(){
+	var el=document.getElementById('boundary1');
+	el.addEventListener("mouseover",function(){
+		el.setAttribute('class','boundary youlose');
+		console.log("hov");
 		});
-	
-	
-		var glory=document.querySelectorAll('div.boundary');
-		var i = glory.length- 2;
-		for (i; i >= 0; i--) {
-			//Adds listener to each div.
-			glory[i].addEventListener('mouseover',function(){
-				glory[1].setAttribute('class' ,'boundary youlose');
-				glory[2].setAttribute('class' ,'boundary youlose');
-				glory[3].setAttribute('class' ,'boundary youlose');
-				glory[4].setAttribute('class' ,'boundary youlose');
-				glory[5].setAttribute('class' ,'boundary youlose');
-				glory[0].setAttribute('class' ,'boundary youlose');
-				});
-				
-			};
-	
-} 
+}
+
+function ex2(){
+	var glory=document.querySelectorAll('div.boundary');
+	var i = glory.length- 2;
+	for (i; i >= 0; i--) {
+		//Adds listener to each div.
+		glory[i].addEventListener('mouseover',function(){			
+			//glory[i].setAttribute('class','boundary youlose');
+			/*glory[1].setAttribute('class' ,'boundary youlose');
+			glory[2].setAttribute('class' ,'boundary youlose');
+			glory[3].setAttribute('class' ,'boundary youlose');
+			glory[4].setAttribute('class' ,'boundary youlose');
+			glory[0].setAttribute('class' ,'boundary youlose');*/
+			l+=1;//ex3 (increments whenever the borders are touched)
+		});		
+	};
+	ex3();
+}
+
+function ex3(){
+	var end=document.getElementById('end');
+	end.addEventListener('mouseover', function(){
+		if(l==1){ //checks whether the borders were touched before getting to the end
+			alert("you Lose!");
+		}
+		else{
+			alert("You Won!");
+		}
+	});
+}
+
+
+		
